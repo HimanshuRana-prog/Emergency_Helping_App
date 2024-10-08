f559702741de9e4d1f28330ac50b80c0e968730b
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-08 15:25:40 +0530

    added

---
+++ gui.py
@@ -0,0 +1,11 @@
+import PySimpleGUI as sg
+import webbrowser
+
+webbrowser.open("https://www.google.co.in/maps")
+
+sg.theme("Black")
+
+lable1 = sg.Text("GPS")
+input_1 = sg.Input()
+
+button
---
+++ maps.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+
+
+f = webbrowser.open("https://www.google.co.in/maps" )
+
+print(f)
